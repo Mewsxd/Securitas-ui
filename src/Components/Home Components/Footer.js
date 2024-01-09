82eaf1ce7705850e4ae8a92639924a19a5c7a854
Muzammil Malik <abdmalik1593@gmail.com>
2024-01-09 13:25:35 +0530

    changes

--- src/Components/Home Components/Footer.js
+++ src/Components/Home Components/Footer.js
@@ -7,7 +7,7 @@ import { AiFillInstagram } from "react-icons/ai";
 import logo from "../../Assets/logo.png";
 const Footer = () => {
   return (
-    <div className={classes.container}>
+    <div className={classes.footerContainer}>
       <div className={classes.innerContainer}>
         <div className={classes.left}>
           <div className={classes.logo}>
@@ -50,9 +50,21 @@ const Footer = () => {
               <li>+91 8169017703, +91 7021840281</li>
             </ul>
             <div className={classes.socialMediaIcons}>
-              <BsFacebook />
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/BetiHaii"
+              >
+                <BsFacebook />
+              </a>
               <GrYoutube />
-              <AiFillInstagram />
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/betihaii"
+              >
+                <AiFillInstagram />
+              </a>
             </div>
           </div>
         </div>
